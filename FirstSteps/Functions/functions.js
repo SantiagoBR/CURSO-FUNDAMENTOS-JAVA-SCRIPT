9268af2f4b000975b74b623feb25b2edfdf8c8b8
Santiago <santiagobastidasrodriguez@gmail.com>
2021-05-02 21:24:49 -0500

    Function Class, with examples

---
+++ FirstSteps/Functions/functions.js
@@ -0,0 +1,9 @@
+var Name = 'santiago', Age = 19
+function printAge(n, a)
+{
+    console.log(`${n} tiene ${a} años`)
+}
+printAge(Name, Age)
+printAge('Viky', 28)
+printAge('Erick', 32)
+printAge('Darío', 27)
