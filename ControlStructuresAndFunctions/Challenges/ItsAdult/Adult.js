21cdc77941c2123f9db275005982bbda05d2693c
Santiago <santiagobastidasrodriguez@gmail.com>
2021-05-20 09:04:18 -0500

    Challenge con funciones que retornan

--- ControlStructuresAndFunctions/Challenges/ItsAdult/Adult.js
+++ ControlStructuresAndFunctions/Challenges/ItsAdult/Adult.js
@@ -4,19 +4,25 @@ var person =
     Age: prompt('¿Cuál es tu edad?') 
 }
 
+const AGE_ADULT = 18
 
-function ItsAdult(person) {
+function IsAdult(person)
+{
+   return person.Age >= AGE_ADULT
+}
+function printIfIsAdult(person) 
+{
     var { Name, Age } = person     
 
     console.log(`Tu nombre es ${Name} y tu tienes ${Age} años`)
 
-    if(Age < 18)
+    if(IsAdult(person))
     {
-        console.log('Eres menor de edad')
+        console.log('Eres mayor de edad')
     }
     else
     {
-        console.log('Eres mayor de edad')
+        console.log('Eres menor de edad')
     }
 }
 
