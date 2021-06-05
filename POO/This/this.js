c106d3e405bb6ff35a1eca14a59e384ecc4695cc
Santiago <santiagobastidasrodriguez@gmail.com>
2021-06-04 23:11:28 -0500

    'This' explain class

--- POO/This/this.js
+++ POO/This/this.js
@@ -1,7 +1,7 @@
 const TALL = 1.65
-let UserName = prompt('¿Cuál es tu nombre?')
-let UserLastName = prompt('¿Cuál es tu apellido?')
-let UserHeight = prompt('¿Cuál es tu altura?')
+let UserName = prompt("¿Cuál es tu nombre?")
+let UserLastName = prompt("¿Cuál es tu apellido?")
+let UserHeight = prompt("¿Cuál es tu altura?")
 function Person(Name, LastName, Height) {
     this.Name = Name
     this.LastName = LastName
@@ -11,9 +11,12 @@ function Person(Name, LastName, Height) {
 let User = new Person(UserName, UserLastName, UserHeight)
 
 let IsTall = User.Height >= TALL ? `soy alt@` : `no soy alt@`
+
 Person.prototype.Salute = function () {
-    console.log(`Hola me llamo ${this.Name} ${this.LastName} y ${IsTall} porque mido ${this.Height} mts`)
+    return console.log(`Hola me llamo ${this.Name} ${this.LastName} y ${IsTall} porque mido ${this.Height} mts`)
 }
+
+console.log(this.Person.Height)
 
 User.Salute()
 
